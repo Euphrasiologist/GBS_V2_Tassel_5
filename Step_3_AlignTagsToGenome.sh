0440a4ee56774e982bd03fa54b0f0cef67cc0490
Max Brown <max.brown@ed.ac.uk>
2020-02-11 14:33:56 +0000

    Adding next script.

--- Step_3_AlignTagsToGenome.sh
+++ Step_3_AlignTagsToGenome.sh
@@ -5,20 +5,22 @@
 
 echo -e "\n\n Activate conda bwa \n\n"
 
-~/miniconda3/condabin/conda activate bwa
+source ~/miniconda3/etc/profile.d/conda.sh 
+
+conda activate bwa
 
 # if indexing is necessary do it now
 # e.g. bwa index [reference genome]
 
 echo -e "\n\n Index the reference genome \n\n"
 
-bwa index -a bwts ../scaffolds_disomic_concat_dipOrTet.fasta.gz 2>&1 
+bwa index -a bwtsw ../scaffolds_disomic_concat_dipOrTet.fasta.gz
 
 # align to the reference
 
 echo -e "\n\n Align the reads to the reference genome \n\n"
 
-bwa aln -t 15 ../scaffolds_disomic_concat_dipOrTet.fasta.gz ../alignment/tagstoalign.fa.gz > ../alignment/tagstoalign.sai 2>&1 
+bwa aln -t 15 ../scaffolds_disomic_concat_dipOrTet.fasta.gz ../alignment/tagstoalign.fa.gz > ../alignment/tagstoalign.sai
 
 # and convert the output into a human readable .sam file
 
@@ -26,6 +28,6 @@ bwa aln -t 15 ../scaffolds_disomic_concat_dipOrTet.fasta.gz ../alignment/tagstoa
 
 bwa samse ../scaffolds_disomic_concat_dipOrTet.fasta.gz ../alignment/tagstoalign.sai ../alignment/tagstoalign.fa.gz > ../alignment/tagstoalign.sam 
 
-~/miniconda3/condabin/conda deactivate
+conda deactivate
 
-echo print -e "\n\n Done! \n\n"
+echo -e "\n\n Done! \n\n"
---
+++ Step_4_SAMToGBSdbPlugin.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Step four of the GBS pipeline is to read the SAM file generated to determine where
+# the postitions are on the reference genome.
+
+# there are other flags to consider, but I leave them all default.
+
+# set up the log again
+LOG=./logs/SAMToGBSdbPlugin.log
+
+# the command
+
+echo -e "\n\n Finding positions of tags on the reference genome \n\n"
+
+../tassel-5-standalone/run_pipeline.pl -fork1 -SAMToGBSdbPlugin -i ../alignment/tagstoalign.sam -db ../database_files/gbs.db -endPlugin -runfork1
